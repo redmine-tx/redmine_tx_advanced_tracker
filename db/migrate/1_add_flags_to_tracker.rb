1a79b3500ad2ffd8717c985510b47d2b2726f6c3
KiHyun Kang <testors@gmail.com>
2026-01-29 17:09:45 +0900

    Update redmine_tx_advanced_tracker - 2026-01-29 17:09:45

--- db/migrate/1_add_flags_to_tracker.rb
+++ db/migrate/1_add_flags_to_tracker.rb
@@ -1,17 +1,15 @@
 class AddFlagsToTracker < ActiveRecord::Migration[5.2]
-    def up
-      add_column :trackers, :is_sidejob, :boolean, null: false, default: false
-      add_column :trackers, :is_bug, :boolean, null: false, default: false
-      add_column :trackers, :is_patchnote, :boolean, null: false, default: false
-      add_column :trackers, :is_exception, :boolean, null: false, default: false
-    end
-  
-    def down
-      remove_column :trackers, :is_sidejob
-      remove_column :trackers, :is_bug
-      remove_column :trackers, :is_patchnote
-      remove_column :trackers, :is_exception
-    end
-  end 
-    
-  
+  def up
+    add_column :trackers, :is_sidejob, :boolean, null: false, default: false unless column_exists?(:trackers, :is_sidejob)
+    add_column :trackers, :is_bug, :boolean, null: false, default: false unless column_exists?(:trackers, :is_bug)
+    add_column :trackers, :is_patchnote, :boolean, null: false, default: false unless column_exists?(:trackers, :is_patchnote)
+    add_column :trackers, :is_exception, :boolean, null: false, default: false unless column_exists?(:trackers, :is_exception)
+  end
+
+  def down
+    remove_column :trackers, :is_sidejob if column_exists?(:trackers, :is_sidejob)
+    remove_column :trackers, :is_bug if column_exists?(:trackers, :is_bug)
+    remove_column :trackers, :is_patchnote if column_exists?(:trackers, :is_patchnote)
+    remove_column :trackers, :is_exception if column_exists?(:trackers, :is_exception)
+  end
+end
